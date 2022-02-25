2aab761e7bb067b033ae26282023309b8808e020
kingannuj <63520863+kingannuj@users.noreply.github.com>
2022-02-25 10:00:01 +0530

    initial commit

--- src/main/java/com/jenkis/jenkinscicd/JenkinscicdApplication.java
+++ src/main/java/com/jenkis/jenkinscicd/JenkinscicdApplication.java
@@ -2,7 +2,7 @@ package com.jenkis.jenkinscicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.io.*;
+
 
 
 @SpringBootApplication
