a3608be452ce3dfee22c7eb0f64e2d8cfe6cb5b2
kingannuj <63520863+kingannuj@users.noreply.github.com>
2022-02-25 09:55:50 +0530

    first commit

---
+++ src/main/java/com/jenkis/jenkinscicd/JenkinscicdApplication.java
@@ -0,0 +1,16 @@
+package com.jenkis.jenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.*;
+
+
+@SpringBootApplication
+public class JenkinscicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinscicdApplication.class, args);
+		System.out.println("Hii Intelije");
+	}
+
+}
